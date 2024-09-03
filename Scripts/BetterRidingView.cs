695ff92f3b6cba3cb6f0a519262c7fe5156d8707
Arshvir Goraya <113562877+ArshvirGoraya@users.noreply.github.com>
2024-09-03 17:39:24 -0400

    dynamic horizontal positioning and preset values

--- Scripts/BetterRidingView.cs
+++ Scripts/BetterRidingView.cs
@@ -15,18 +15,18 @@ using System.Collections.Generic;
 using System;
 using __ExternalAssets;
 using System.Runtime.InteropServices.WindowsRuntime;
+using DaggerfallWorkshop.Game.Serialization;
 
 namespace BetterRidingViewMod
 {
     public class BetterRidingView : MonoBehaviour
     {
-        // * Dynamic Positiong
+        // * Vertical Camera Positioning:
         public static bool dynamic_horse_positioning = true;
         public static float horse_center_position = 0;
         public static float horse_center_angle = 0; // -15
         public static float horse_down_position = 0;
         public static float horse_down_angle = 0; // 40
-        public static float horse_horizontal_position = 0;
         // * Dynamic Jumping
         public static bool dynamic_horse_jumping = true;
         public static float max_horse_jump_height = 0;
@@ -44,6 +44,14 @@ namespace BetterRidingViewMod
         public float current_tween_value = 0;
         float tween_elapsed_time = 0;
         public bool on_ground = true;
+        // * Horizontal Camera Positioning:
+        public static float horizontal_lerp_strength = 0.1f;
+        public static float horse_horizontal_position_target = 0;
+        public static float horse_horizontal_position = 0;
+        GameObject gameObjectPlayerAdvanced = null;
+        public float previous_camera_y_angle = 0;
+        public bool previous_on_foot = true;
+
         // * Camera
         float camera_angle_x = 0;
         float normalized_angle_x = 0;
@@ -67,13 +75,12 @@ namespace BetterRidingViewMod
         }
         // * Raised when user changes mod settings.
         static void LoadSettings(ModSettings modSettings, ModSettingsChange change){
-            // * Vertical Positioning:
+            // * Vertical Camera Positioning:
             dynamic_horse_positioning = modSettings.GetBool("DynamicHorsePositioning", "DynamicHorsePositioning");
             horse_center_position = modSettings.GetFloat("CenterPositioning", "HorseCenterPosition");
             horse_center_angle = modSettings.GetFloat("CenterPositioning", "HorseCenterAngle");
             horse_down_position = modSettings.GetFloat("DownPositioning", "HorseDownPosition");
             horse_down_angle = modSettings.GetFloat("DownPositioning", "HorseDownAngle");
-            horse_horizontal_position = modSettings.GetFloat("HorizontalPositioning", "HorseHorizontalPosition");
             // * Jumping:
             dynamic_horse_jumping = modSettings.GetBool("DynamicJumping", "DynamicHorseJumping");
             max_horse_jump_height = modSettings.GetFloat("DynamicJumping", "MaxJumpHeight");
@@ -81,37 +88,83 @@ namespace BetterRidingViewMod
             horse_jump_down_time = modSettings.GetFloat("DynamicJumping", "JumpDownTime");
             easing_up_function_num = modSettings.GetInt("DynamicJumping", "JumpUpEasing");
             easing_down_function_num = modSettings.GetInt("DynamicJumping", "JumpDownEasing");
+            // * Horizontal Camera Positioning:
+            horse_horizontal_position_target = modSettings.GetFloat("HorizontalPositioning", "HorseHorizontalPosition");
+            horizontal_lerp_strength = (float)modSettings.GetInt("HorizontalPositioning", "LerpStrength") / 100; // Make into float.
+            if (horizontal_lerp_strength < 1){
+                horse_horizontal_position = horse_horizontal_position_target;
+            }
         }
         private void Start()
         {
+            // SaveLoadManager.OnLoad += Load;
             GameManager.Instance.TransportManager.DrawHorse = false;
+            previous_on_foot = GameManager.Instance.TransportManager.IsOnFoot;
+            gameObjectPlayerAdvanced = GameObject.Find("PlayerAdvanced");
         }
-        public static float NormalizeValue(float value, float min, float max)
-        {
+        // private void Load(SaveData_v1 saveData){
+        //     previous_on_foot = GameManager.Instance.TransportManager.IsOnFoot;
+        // }
+////////////////////////////////////////////////////////////////////////////////
+        public static float NormalizeValue(float value, float min, float max){
             return (value - min) / (max - min);
         }
-        public static float GetValueFromNormalize(float normalized_value, float min, float max)
-        {
+        public static float GetValueFromNormalize(float normalized_value, float min, float max){
             return min + normalized_value * (max - min);
         }
-        public float NormalizeTo180Angle(float angle)
-        {
+        public float NormalizeTo180Angle(float angle){
             if (angle > 180)
                 angle -= 360;
             return angle;
         }
-
+        public float GetYRotationDifference(){
+            float diff = previous_camera_y_angle - gameObjectPlayerAdvanced.transform.eulerAngles.y;
+            if (diff > 180f){diff -= 360f;}
+            else if (diff < -180f){diff += 360f;}
+            return diff;
+        }
+        public void EnteredRiding(){
+            previous_camera_y_angle = gameObjectPlayerAdvanced.transform.eulerAngles.y;
+            horse_horizontal_position = horse_horizontal_position_target;
+        }
+////////////////////////////////////////////////////////////////////////////////
         public float GetHorseTextureOffset(){
             camera_angle_x = -NormalizeTo180Angle(GameManager.Instance.MainCamera.transform.eulerAngles.x);
             camera_angle_x = Mathf.Clamp(camera_angle_x, horse_center_angle, horse_down_angle);
             normalized_angle_x = NormalizeValue(camera_angle_x, horse_center_angle, horse_down_angle);
             return GetValueFromNormalize(normalized_angle_x, horse_center_position, horse_down_position);
         }
-
+        float IncrementTweenUp(){
+            float start_val = GetHorseTextureOffset();
+            float target_val = Mathf.Min(horse_center_position, start_val + max_horse_jump_height);
+            if (tween_elapsed_time > horse_jump_up_time) tween_elapsed_time = horse_jump_up_time;
+            current_tween_value = GetValueFromNormalize(
+                BetterRidingViewEasing.Interpolate(NormalizeValue(tween_elapsed_time, 0, horse_jump_up_time), easing_up_function_num),
+                start_val,
+                target_val
+            );
+            return Mathf.Min(horse_center_position, current_tween_value);
+        }
+        float IncrementTweenDown(){
+            float start_val = current_tween_value;
+            float target_val = GetHorseTextureOffset();
+            if (tween_elapsed_time > horse_jump_down_time) tween_elapsed_time = horse_jump_down_time;
+            current_tween_value = GetValueFromNormalize(
+                BetterRidingViewEasing.Interpolate(NormalizeValue(tween_elapsed_time, 0, horse_jump_down_time), easing_down_function_num),
+                start_val,
+                target_val
+            );
+            return Mathf.Min(horse_center_position, current_tween_value);
+        }
+////////////////////////////////////////////////////////////////////////////////
         private void Update(){
-            if (!GameManager.Instance.StateManager.GameInProgress){
+            if (!GameManager.Instance.StateManager.GameInProgress || GameManager.IsGamePaused){
                 return;
             }
+            if (previous_on_foot && !GameManager.Instance.TransportManager.IsOnFoot){
+                EnteredRiding();
+            }
+            previous_on_foot = GameManager.Instance.TransportManager.IsOnFoot;
             if (GameManager.Instance.TransportManager.IsOnFoot){
                 return;
             }
@@ -123,7 +176,6 @@ namespace BetterRidingViewMod
                 if (on_ground){
                     if ((GameManager.Instance.AcrobatMotor.Jumping || GameManager.Instance.AcrobatMotor.Falling)){
                         // * Just Jumped
-                        Debug.Log($"off ground!!!");
                         horse_tween_type = HorseTweenType.TweenUp;
                         on_ground = false;
                         tween_elapsed_time = 0;
@@ -131,14 +183,13 @@ namespace BetterRidingViewMod
                 }else{
                     if (!(GameManager.Instance.AcrobatMotor.Jumping || GameManager.Instance.AcrobatMotor.Falling)){
                         // * Landed from Jump.
-                        Debug.Log($"on ground");
                         horse_tween_type = HorseTweenType.TweenDown;
                         on_ground = true;
                         tween_elapsed_time = 0;
                     }
                 }
             }
-
+            
             if (horse_tween_type == HorseTweenType.None){
                 horse_texture_offset_y = GetHorseTextureOffset();
             }
@@ -154,39 +205,18 @@ namespace BetterRidingViewMod
                     }
                 }
             }
-            if (horse_texture_offset_y > horse_center_position){
-                Debug.Log($"GOING OVER THE LIMIt: {horse_texture_offset_y} > {horse_center_position}");
+            // * Dynamic Horizontal Positioning
+            if (horizontal_lerp_strength < 1){
+                float camera_horizontal_diff = GetYRotationDifference();
+                previous_camera_y_angle = gameObjectPlayerAdvanced.transform.eulerAngles.y;
+                horse_horizontal_position += camera_horizontal_diff;
+                horse_horizontal_position = Mathf.Lerp(
+                    horse_horizontal_position,
+                    horse_horizontal_position_target,
+                    horizontal_lerp_strength
+                );
             }
         }
-
-
-        float IncrementTweenUp(){
-            float start_val = GetHorseTextureOffset();
-            float target_val = Mathf.Min(horse_center_position, start_val + max_horse_jump_height);
-            if (tween_elapsed_time > horse_jump_up_time) tween_elapsed_time = horse_jump_up_time;
-            current_tween_value = GetValueFromNormalize(
-                BetterRidingViewEasing.Interpolate(NormalizeValue(tween_elapsed_time, 0, horse_jump_up_time), easing_up_function_num),
-                start_val,
-                target_val
-            );
-            // return current_tween_value;
-            return Mathf.Min(horse_center_position, current_tween_value);
-        }
-
-        float IncrementTweenDown(){
-            float start_val = current_tween_value;
-            float target_val = GetHorseTextureOffset();
-            // float target_val = Mathf.Min(horse_center_position, GetHorseTextureOffset());
-            if (tween_elapsed_time > horse_jump_down_time) tween_elapsed_time = horse_jump_down_time;
-            current_tween_value = GetValueFromNormalize(
-                BetterRidingViewEasing.Interpolate(NormalizeValue(tween_elapsed_time, 0, horse_jump_down_time), easing_down_function_num),
-                start_val,
-                target_val
-            );
-            // return current_tween_value;
-            return Mathf.Min(horse_center_position, current_tween_value);
-        }
-
         // * Mimics the OnGUI method inside of TransportManager.cs, with some additions to allow dynamic horse positioning.
         void OnGUI(){
             if (!GameManager.Instance.StateManager.GameInProgress){
